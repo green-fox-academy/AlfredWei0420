61c4f1567eb4c511a459b16350f1cf6fb72e9d23
Alfred Wei <35080930@qq.com>
2017-08-30 11:54:24 +0800

    corrected the repo name

---
+++ week-02/day-03/ex2.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I like trains!"
+var i = 0;
+
+while (i<100) {
+    console.log("I like trains"+i);
+    i++;
+}
---
+++ week-02/day-03/ex3.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+/*var i = 0;
+while((i%2==0)||(i<500)){
+    console.log(i);
+    i=i+2;
+}*/
+
+for(vari=0;i<500;i++){
+    if(i%2===0){
+        console.log(i)
+    }
+}
